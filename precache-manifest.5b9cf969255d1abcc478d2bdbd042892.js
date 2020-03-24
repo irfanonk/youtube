8f795476358eddd992229599d18831b5a6958980
irfan <onk.irfan@gmail.com>
2020-03-24 16:05:55 +0300

    Updates

---
+++ precache-manifest.5b9cf969255d1abcc478d2bdbd042892.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0c53a4253344cd5286551cdd0acd51",
+    "url": "/youtube/index.html"
+  },
+  {
+    "revision": "14b1c263c6745a77cf1b",
+    "url": "/youtube/static/js/2.879afb72.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/youtube/static/js/2.879afb72.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3124d7cba534dea3393",
+    "url": "/youtube/static/js/main.b8d8d6f6.chunk.js"
+  },
+  {
+    "revision": "49bb040344d3da7a3afb",
+    "url": "/youtube/static/js/runtime-main.e5cde201.js"
+  }
+]);
